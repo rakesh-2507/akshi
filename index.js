92cacbc9e4a33ec231e933c4771a51e537f6ca11
Rakesh <rakeshperumalapalli1@gmail.com>
2025-07-26 15:22:40 +0530

    fix: use dynamic port for Render

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(10000, () => {
-  console.log(`🚀 Server running on port 10000`);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
